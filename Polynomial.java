3557efe442cd7193af63439d8469ae9d8e3ee8b3
Zuhair Khan <mzkpkcan@gmail.com>
2023-09-18 15:12:44 -0400

    added polynomial.java and Driver.java

---
+++ Polynomial.java
@@ -0,0 +1,37 @@
+public class Polynomial{
+    private double[] coefficients;
+
+    public Polynomial(){
+        coefficients = new double[1];
+    }    
+
+    public Polynomial(double[] given_coeff) {
+        coefficients = given_coeff;
+    }
+
+    public Polynomial add(Polynomial p3) {
+        int maxlength = Math.max(coefficients.length, p3.coefficients.length);
+        double[] result = new double[maxlength];
+
+        for ( int i = 0; i < coefficients.length; i++){
+            result[i] += coefficients[i];
+        }
+
+        for (int j = 0; j < p3.coefficients.length; j++){
+            result[j] += p3.coefficients[j];
+        }
+        return new Polynomial(result);
+    }
+
+    public double evaluate(double x){
+        double value = 0;
+        for (int i = 0; i < coefficients.length; i++){
+            value += Math.pow(x, i) * coefficients[i];
+        }
+        return value;
+    }
+
+    public boolean hasRoot(double x){
+        return evaluate(x) == 0;
+    }
+}
